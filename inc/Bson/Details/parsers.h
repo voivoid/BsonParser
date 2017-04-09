2e1fe8a9ca87b1177f439e5d5805d90c14de6380
Sergey Ivanov <voivoid@mail.ru>
2017-04-09 21:47:33 +0300

    explicit instantiations for readers

--- inc/Bson/Details/parsers.h
+++ inc/Bson/Details/parsers.h
@@ -16,12 +16,33 @@ T read(Istream& stream);
 
 template <typename T>
 typename std::enable_if_t<std::is_pod<T>::value> write(T value, Ostream& stream);
+
+template <>
+String read(Istream& stream);
 void write(const String& string, Ostream& stream);
+
+template <>
+True read(Istream& stream);
 void write(True, Ostream& stream);
+
+template <>
+False read(Istream& stream);
 void write(False, Ostream& stream);
+
+template <>
+CString read(Istream& stream);
 void write(const CString& string, Ostream& stream);
+
+template <>
+Element read(Istream& stream);
 void write(const Element& element, Ostream& stream);
+
+template <>
+List read(Istream& stream);
 void write(const List& list, Ostream& stream);
+
+template <>
+Document read(Istream& stream);
 void write(const Document& doc, Ostream& stream);
 void write(const Document& doc, Ostream& stream, size_t documentSize);
 
